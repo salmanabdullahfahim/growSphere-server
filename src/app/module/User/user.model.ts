083bbd7759142cd01b7ff33fd109adb965623f3b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-28 20:00:12 +0600

    model & interface:  Added user model and interface

---
+++ src/app/module/User/user.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+import { TUser, UserModel } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../../config";
+
+const userSchema = new Schema<TUser, UserModel>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  profileImage: { type: String, required: true },
+  password: { type: String, required: true, select: 0 },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
+
+// password hashing
+userSchema.pre("save", async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  next();
+});
+
+// remove password field from response
+userSchema.post("save", function (doc, next) {
+  doc.password = undefined;
+
+  next();
+});
+
+userSchema.statics.isUserExistsByEmail = async function (email: string) {
+  return await User.findOne({ email }).select("+password");
+};
+
+userSchema.statics.isPasswordMatched = async function (
+  planeTextPassword: string,
+  hashedPassword: string
+) {
+  return await bcrypt.compare(planeTextPassword, hashedPassword);
+};
+
+export const User = model<TUser, UserModel>("User", userSchema);
---
+++ src/app/module/User/user.validation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const userValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: "Name is required" }),
+    email: z.string({ required_error: "Email is required" }).email(),
+    profileImage: z.string({ required_error: "Profile Image is required" }),
+    password: z.string({ required_error: "Password is required" }),
+    phone: z.string({ required_error: "Phone Number is required" }),
+    address: z.string({ required_error: "Address is required" }),
+    role: z.enum(["admin", "user"]),
+  }),
+});
+
+export const userValidations = {
+  userValidationSchema,
+};
