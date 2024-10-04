1c97275af9205e07ebe1c327cd678bbe3e789a5d
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-10-04 21:50:29 +0600

    add: timestamps to the payment model

--- src/app/module/Payment/payment.model.ts
+++ src/app/module/Payment/payment.model.ts
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-const PaymentSchema = new mongoose.Schema({
-  transactionId: {
-    type: String,
-    required: true,
+const PaymentSchema = new mongoose.Schema(
+  {
+    transactionId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const Payment = mongoose.model("Payment", PaymentSchema);
